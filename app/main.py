105255986f90e0f14660764d5116e6d8d059dbd6
Lukasz Kmiecik <kmieciklu@gmail.com>
2025-09-10 09:23:45 +0200

    Implement Person class and create_person_list function2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 class Person:
-
     people = {}  # name: Person
-
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
@@ -10,28 +8,20 @@ class Person:
 
 def create_person_list(people: list) -> list:
     Person.people.clear()
-
     result_list = []
-
-
     for person in people:
         temp_person = Person(person["name"], person["age"])
         result_list.append(temp_person)
-
-
     for person in people:
         me = Person.people[person["name"]]
-
         if "wife" in person and person["wife"] is not None:
             spouse = Person.people[person["wife"]]
             me.wife = spouse
             if not hasattr(spouse, "husband"):
                 spouse.husband = me
-
         if "husband" in person and person["husband"] is not None:
             spouse = Person.people[person["husband"]]
             me.husband = spouse
             if not hasattr(spouse, "wife"):
                 spouse.wife = me
-
     return result_list
